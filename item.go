6a42068813faa438d98b83c2762f06891440e9b8
TheMrCake <antonlichota@gmail.com>
2022-08-22 17:48:58 -0400

    Second commit

--- item.go
+++ item.go
@@ -1,14 +1,20 @@
 package main
 
+import "fmt"
+
 type item struct {
 	Name, Description, Barcode string
-	Quantity                   int16
-	MinQuantity                int16
+	Quantity                   int
+	MinQuantity                int
 }
 
-func newItem(n string, des string, bd string, q int16, min int16) *item {
+func newItem(n string, des string, bd string, q int, min int) *item {
 	i := item{Name: n, Description: des, Barcode: bd, Quantity: q, MinQuantity: min}
 	return &i
 }
 
-func add()
+func add(i *item, n int) {
+	i.Quantity += n
+	fmt.Printf("Added %v, to %s", n, i.Name)
+
+}
